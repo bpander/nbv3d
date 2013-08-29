bd6ebc49ac1562862377bac9d4dd89ceaf0e738a
Brad Anderson <brad.anderson87@gmail.com>
2013-08-29 14:23:47 -0500

    Allowing settings.js to be used between node-side and client-side

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,3 +1,30 @@
 window.SETTINGS = {};
 
 SETTINGS.GAME_SERVER_ADDRESS = 'http://192.168.1.133';
+
+var config = function (requirejs) {
+    requirejs.config({
+
+        paths: {
+            'three': 'vendor/three.min',
+            'physijs': 'vendor/physi',
+            'socket.io': 'vendor/socket.io-client/socket.io',
+            'jquery': 'vendor/jquery-2.0.3.min'
+        },
+
+        shim: {
+            'physijs': ['three']
+        },
+
+        deps: [
+            'settings'
+        ]
+
+    });
+};
+
+if (typeof module === 'undefined') {
+    config(requirejs);
+} else {
+    module.exports = config;
+}
