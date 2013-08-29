75ff41f1dedafcd502d6271bd076fbacad6fc15e
Brad Anderson <brad.anderson87@gmail.com>
2013-08-29 13:17:33 -0500

    Switching app.js over to requirejs syntax

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var path = require('path');
-var ejs = require('ejs');
-var GameServer = require('./GameServer.js');
+var requirejs = require('requirejs');
 
-// Configuration
-app.set('port', process.env.TEST_PORT || 80);
-app.use(express.static(path.join(__dirname, '/')));
-app.engine('html', ejs.renderFile);
+requirejs([
+    'express',
+    'http',
+    'path',
+    'ejs',
+    './GameServer'
+], function (
+    express,
+    http,
+    path,
+    ejs,
+    GameServer
+) {
 
-// Routes
-app.get('/', function (req, res) {
-    res.render(__dirname + '/views/index.html');
-});
+    var app = express();
+    var server = http.createServer(app);
 
-server.listen(app.get('port'), function () {
-    new GameServer().start(server);
-    console.log('Express server listening on port ' + app.get('port'));
+    // Configuration
+    app.set('port', process.env.TEST_PORT || 80);
+    app.use(express.static(path.join(__dirname, '/')));
+    app.engine('html', ejs.renderFile);
+
+    // Routes
+    app.get('/', function (req, res) {
+        res.render(__dirname + '/views/index.html');
+    });
+
+    server.listen(app.get('port'), function () {
+        new GameServer().start(server);
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+
 });
